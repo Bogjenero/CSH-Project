b0b4d0ac44f186deb8d1c0c6366112b40a247bd0
Bogjenero <bornagjenero1@gmail.com>
2023-12-08 19:10:14 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(25, 55);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(202, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Unesite put do foldera";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(277, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 27);
+            button1.TabIndex = 2;
+            button1.Text = "Browse";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(25, 96);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(119, 19);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Hidden";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(124, 190);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(380, 183);
+            listView1.TabIndex = 4;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listView1);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private CheckBox checkBox1;
+        private ListView listView1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool provjera = true;
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.Columns.Add("Ime datoteke", 150);
+            listView1.Columns.Add("Veličina", 100);
+            listView1.Columns.Add("Datum/Vrijeme", 150);
+            listView1.ColumnClick += listView1_ColumnClick;
+            listView1.ListViewItemSorter = null;
+        }
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            var items = listView1.Items.Cast<ListViewItem>().ToList();
+            if (e.Column == 0)
+            {
+                items = items.OrderBy(item => item.SubItems[e.Column].Text).ToList();
+            }
+            else if(e.Column == 1)
+            {
+                items = items.OrderBy(item => int.Parse(item.SubItems[e.Column].Text)).ToList();
+            }
+            else
+            {
+                items = items.OrderBy(item => DateTime.Parse(item.SubItems[e.Column].Text)).ToList();
+            }
+            listView1.Items.Clear();
+            listView1.Items.AddRange(items.ToArray());
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void InformacijeDatoteka()
+        {
+            {
+
+                try
+                {
+                    string uneseniPut1 = textBox1.Text;
+                    string uneseniPut = uneseniPut1.TrimStart('"').TrimEnd('"');
+                    
+                    if (checkBox1.Checked)
+                    {
+                        string[] datoteke = Directory.GetFiles(uneseniPut, "*.*");
+                        for (int i = 0; i < datoteke.Length; i++)
+                        {
+                            FileInfo fileInfo = new FileInfo(datoteke[i]);
+                            string nazivDatoteke = Path.GetFileName(datoteke[i]);
+                            long velicinaDatoteke = fileInfo.Length;
+                            DateTime vrijemeZadnjeIzmjene = fileInfo.LastWriteTime;
+                            ListViewItem item = new ListViewItem(nazivDatoteke);
+                            item.SubItems.Add(velicinaDatoteke.ToString());
+                            item.SubItems.Add(vrijemeZadnjeIzmjene.ToString());
+                            listView1.Items.Add(item);
+                        }
+                        
+                    }
+                    else
+                    {
+                        string[] datoteke = Directory.GetFiles(uneseniPut, "*.*");
+                        for (int i = 0; i < datoteke.Length; i++)
+                        {
+                            FileAttributes atributi = File.GetAttributes(datoteke[i]);
+                            if ((atributi & FileAttributes.Hidden) == 0 )
+                            {
+                                FileInfo fileInfo = new FileInfo(datoteke[i]);
+                                string nazivDatoteke = Path.GetFileName(datoteke[i]);
+                                long velicinaDatoteke = fileInfo.Length;
+                                DateTime vrijemeZadnjeIzmjene = fileInfo.LastWriteTime;
+                                ListViewItem item = new ListViewItem(nazivDatoteke);
+                                item.SubItems.Add(velicinaDatoteke.ToString());
+                                item.SubItems.Add(vrijemeZadnjeIzmjene.ToString());
+                                listView1.Items.Add(item);
+                            }
+                        }
+                     }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Došlo je do greške: " + ex.Message);
+
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && provjera)
+            {
+                provjera = false;
+                InformacijeDatoteka();
+            }
+            else if(textBox1.Text != "" && provjera == false)
+            {
+                MessageBox.Show("Već ste pretražili folder");
+            }     
+            else
+            {
+                MessageBox.Show("Niste unjeli putanju");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
